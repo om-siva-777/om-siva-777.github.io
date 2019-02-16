adb0b4a1dd6ce3fc47ee1380b1fa1d3ea2802462
Harshad <harshadreddy16@gmail.com>
2019-02-15 21:25:51 -0500

    populating testimony

--- js/testimony.js
+++ js/testimony.js
@@ -1,77 +1,32 @@
 $(function() {
 
-  $("#reviewForm input,#reviewForm textarea").jqBootstrapValidation({
-    preventSubmit: true,
-    submitError: function($form, event, errors) {
-      // additional error messages or events
-    },
-    submitSuccess: function($form, event) {
-      event.preventDefault(); // prevent default submit behaviour
-      // get values from FORM
-      var name = $("input#name").val();
-      var message = $("textarea#message").val();
-      var firstName = name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
+	var folder = "../img/review/";
 
-      var file = $("input#imagefile").get(0).files[0];
-      var form = new FormData();
-      form.append('imagefile', file);
-      form.append('name', name);
-      form.append('message',message);
+	$.ajax({
+	    url : folder,
+	    type: "GET",
+	    success: function (data) {
+	        $(data).find("a").attr("href", function (i, val) {
+	            if( val.match(/\.(jpe?g|png|gif)$/) ) {
+	            	var suffix= val.match(/\d+/); 
+	            	$("#testi_slider").html("<div class='shadow-lg testi_item'><div class='row'>") 
+	                $("#testi_slider").append("<div class='col-lg-4'><img class='img-thumbnail' src="+folder+val+" alt='Review Image'></div>")
+	                $("#testi_slider").append("<div class='col-lg-8'><div class='testi_text'><h4 id="+suffix+"></h4><p id="+suffix+"></p></div></div>")
+					$("#testi_slider").append("</div></div>")
+	            } 
+	        });
+	    }
+	});
 
-      $this = $("#sendReviewButton");
-      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      $.ajax({
-        url: "../mail/review.php",
-        type: "POST",
-        cache: false,
-        contentType: false,
-        processData: false,
-        data : form,
-        success: function() {
-          // Success message
-          $('#reviewsuccess').html("<div class='alert alert-success'>");
-          $('#reviewsuccess > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#reviewsuccess > .alert-success')
-            .append("<strong>Thank you for the Review. </strong>");
-          $('#reviewsuccess > .alert-success')
-            .append('</div>');
-          //clear all fields
-          $('#reviewForm').trigger("reset");
-        },
-        error: function() {
-          // Fail message
-          $('#reviewsuccess').html("<div class='alert alert-danger'>");
-          $('#reviewsuccess > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#reviewsuccess > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my server is not responding. Please try again later!"));
-          $('#reviewsuccess > .alert-danger').append('</div>');
-          //clear all fields
-          $('#reviewForm').trigger("reset");
-        },
-        complete: function() {
-          setTimeout(function() {
-            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-          }, 1000);
-        }
-      });
-    },
-    filter: function() {
-      return $(this).is(":visible");
-    },
-  });
-
-  $("a[data-toggle=\"tab\"]").click(function(e) {
-    e.preventDefault();
-    $(this).tab("show");
-  });
-});
-
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-  $('#reviewsuccess').html('');
+	for(var x=1;x>10;x++){	
+		$.ajax({
+	    	url : folder+x+".txt",
+	    	dataType: "text",
+	    	type: "GET",
+	    	success: function (data) {	        
+				$("h4#"+x).text(data.val().split("\n")[0]);
+				$("p#"+x).text(data.replace(/^.*?\r?\n/, ''));
+				}
+			});
+		}
 });
